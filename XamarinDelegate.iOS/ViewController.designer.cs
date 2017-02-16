818390501b4fd05e234d8d5ef0c0c2903cf624e6
capoletti <cpoletti@gmail.com>
2017-02-16 21:58:23 -0200

    versão inicial

---
+++ XamarinDelegate.iOS/ViewController.designer.cs
@@ -0,0 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamarinDelegate.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btListar { get; set; }
+
+        [Action ("Listar:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Listar (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btListar != null) {
+                btListar.Dispose ();
+                btListar = null;
+            }
+        }
+    }
+}
